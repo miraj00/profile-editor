837f53c481ffa0919687bfa055ace8cc99ecc936
miraj <miraj00@gmail.com>
2022-08-29 00:20:58 -0400

    user interface created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { User } from './interfaces/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profileEditor';
+
+  user: User = {
+    name: "Dave",
+    age: 34,
+    favoriteColor: 'black',
+    interests: 'cycling',
+    additionalInfo: 'please fill'
+  }
+
 }
