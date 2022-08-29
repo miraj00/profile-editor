76c88dced3815aea26454759c3210b80e2f5d0af
miraj <miraj00@gmail.com>
2022-08-29 01:45:40 -0400

    Form added and tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,40 @@ import { Component } from '@angular/core';
 
 import { User } from './interfaces/user';
 
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'profileEditor';
+  title = 'User Information';
+
+  // action: string = '';
+
+  displayEdit: boolean = false;
+
+  profileForm: NgForm | undefined;
+
+  toggleEdit(): void {
+    this.displayEdit = !this.displayEdit;
+    if(!this.displayEdit)
+      this.title = "Update User Information"
+    if(this.displayEdit)
+      this.title = "User Information"
+  }
+
+  saveChanges(form: NgForm): void {
+    console.log(this.profileForm?.value)
+  }
 
   user: User = {
-    name: "Dave",
-    age: 34,
-    favoriteColor: 'black',
-    interests: 'cycling',
-    additionalInfo: 'please fill'
+    name: "Miraj",
+    age: 30,
+    favoriteColor: 'Red',
+    interests: 'Hiking',
+    additionalInfo: 'Hobbies'
   }
 
 }
